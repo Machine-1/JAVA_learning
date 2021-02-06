5a0366547e9951cf0a2444309b215fa8f597d885
ziyuan <1115620874@qq.com>
2021-02-06 19:11:48 +0800

    third commit, version3

---
+++ src/Day2021_2_6.java
@@ -0,0 +1,8 @@
+public class Day2021_2_6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i);
+            System.out.println("Today is Day2021_2_6");
+        }
+    }
+}
