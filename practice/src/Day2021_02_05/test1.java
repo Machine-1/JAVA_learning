83ce740d3a327f4bb8a273e423046da1dd126870
ziyuan <1115620874@qq.com>
2021-02-06 18:45:33 +0800

    first commit, version2

--- practice/src/Day2021_02_05/test1.java
+++ practice/src/Day2021_02_05/test1.java
@@ -4,7 +4,7 @@ public class test1 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
-            System.out.println("version1");
+            System.out.println("version2");
         }
     }
 }
